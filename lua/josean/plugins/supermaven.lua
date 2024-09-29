20449a568c63de46c4ea8135db01850e884da7d2
Geisson19 <gs.ponce20@uniandes.edu.co>
2024-09-29 12:01:06 -0500

    use pro supermaven

--- lua/josean/plugins/supermaven.lua
+++ lua/josean/plugins/supermaven.lua
@@ -1,6 +1,12 @@
 return {
 	"supermaven-inc/supermaven-nvim",
 	config = function()
-		require("supermaven-nvim").setup({})
+		require("supermaven-nvim").setup({
+			-- Your other configurations can go here
+		})
+
+		-- Automatically switch to Pro version
+		local api = require("supermaven-nvim.api")
+		api.use_pro()
 	end,
 }
